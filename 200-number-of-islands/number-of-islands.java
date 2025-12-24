3ead92b47db199cf972f5408422c9d4532101c2e
Shreya Kumari <shreyathakur911@gmail.com>
2025-12-24 18:14:16 +0530

    Time: 5 ms (32.45%) | Memory: 51.5 MB (75.43%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,59 @@
+class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int row, int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int count = 0;
+        boolean[][] vis = new boolean[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && vis[i][j]==false){
+                    bfs(i,j,grid,vis);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void bfs(int i, int j, char[][] grid, boolean[][] vis){
+        int m = grid.length, n = grid[0].length;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i,j));
+        while(q.size()>0){
+            Pair front = q.remove();
+            int row = front.row, col = front.col;
+            if(row>0){
+                if(vis[row-1][col]==false && grid[row-1][col]=='1'){
+                    q.add(new Pair(row-1,col));
+                    vis[row-1][col] = true;
+                }
+            }
+            if((row+1)<m){
+                if(vis[row+1][col]==false && grid[row+1][col]=='1'){
+                    q.add(new Pair(row+1,col));
+                    vis[row+1][col] = true;
+                }
+            }
+            if(col>0){
+                if(vis[row][col-1]==false && grid[row][col-1]=='1'){
+                    q.add(new Pair(row,col-1));
+                    vis[row][col-1]=true;
+                }
+            }
+            if((col+1)<n){
+                if(vis[row][col+1]==false && grid[row][col+1]=='1'){
+                    q.add(new Pair(row,col+1));
+                    vis[row][col+1]=true;
+                }
+            }
+        }
+    }
+}
