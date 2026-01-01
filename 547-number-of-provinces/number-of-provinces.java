50bd0128efefa543d7d19bf000983c160562c2e2
Shreya Kumari <shreyathakur911@gmail.com>
2026-01-01 14:29:14 +0530

    Time: 1 ms (86.31%) | Memory: 47.5 MB (29.75%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -5,11 +5,22 @@ class Solution {
         boolean[] vis = new boolean[n];
         for(int i = 0; i<n; i++){
             if(!vis[i]){
-                bfs(i,vis,isConnected);
+                //bfs(i,vis,isConnected);
+                dfs(i,vis,isConnected);
                 count++;
             }
         }
         return count;
+    }
+    
+    public void dfs(int i, boolean[] vis, int[][] isConnected){
+        int n = isConnected.length;
+        vis[i] = true;
+        for(int j=0;j<n;j++){
+            if(isConnected[i][j]==1 && vis[j]==false){
+                dfs(j,vis,isConnected);
+            }
+        }
     }
     public void bfs(int i, boolean[] vis, int[][] isConnected){
         int n = isConnected.length;
