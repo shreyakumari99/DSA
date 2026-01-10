b1a3e479c5d4befd1c4a2ef97a7a230e863a0a67
Shreya Kumari <shreyathakur911@gmail.com>
2026-01-10 22:06:37 +0530

    Time: 7 ms (62.78%) | Memory: 47.3 MB (25.55%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canFinish(int n, int[][] pre) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=1;i<=n;i++) adj.add(new ArrayList<>());
+        int[] indegree = new int[n];
+        boolean[] visited = new boolean[n];
+        for(int i=0;i<pre.length;i++){
+            int a = pre[i][0], b = pre[i][1];
+            adj.get(b).add(a);
+            indegree[a]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.add(i);
+                visited[i] = true;
+            }
+        }
+        while(q.size()>0){
+            int front = q.remove();
+            ans.add(front);
+            for(int ele: adj.get(front)){
+                indegree[ele]--;
+                if(indegree[ele]==0){
+                    q.add(ele); visited[ele]=true;
+                }
+            }
+        }
+        if(ans.size()==n) return true;
+        else return false;
+    }
+}
