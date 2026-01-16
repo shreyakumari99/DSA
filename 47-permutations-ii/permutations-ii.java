d5798f064dbc2f6f73bd0935df366444bad1856a
Shreya Kumari <shreyathakur911@gmail.com>
2026-01-16 21:30:18 +0530

    Time: 3 ms (90.90%) | Memory: 47 MB (28.63%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Sort to handle duplicates
+        backtrack(result, new ArrayList<>(), nums, new boolean[nums.length]);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums, boolean[] used) {
+        if (tempList.size() == nums.length) {
+            result.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])) {
+                    continue; // Skip duplicates
+                }
+                used[i] = true;
+                tempList.add(nums[i]);
+                backtrack(result, tempList, nums, used);
+                used[i] = false;
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
