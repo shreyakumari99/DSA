cd0b4df80c2ef2e3911a48147c901891d3b8bdf5
Shreya Kumari <shreyathakur911@gmail.com>
2026-06-14 18:15:41 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (37.13%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -8,27 +8,47 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-      ListNode dummy = new ListNode(0);
-    ListNode curr = dummy;
-    int carry = 0;
-
-    while (l1 != null || l2 != null || carry > 0) {
-      if (l1 != null) {
-        carry += l1.val;
-        l1 = l1.next;
-      }
-      if (l2 != null) {
-        carry += l2.val;
-        l2 = l2.next;
-      }
-      curr.next = new ListNode(carry % 10);
-      carry /= 10;
-      curr = curr.next;
+        // Dummy head simplifies list creation so we don't have to write 
+        // special logic for the very first node.
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+        
+        // Loop as long as there are nodes in EITHER list, OR we have a leftover carry
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
+            // Calculate the new carry (e.g., 14 / 10 = 1)
+            carry = sum / 10;
+            
+            // Create the new node with the single digit (e.g., 14 % 10 = 4)
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+        }
+        
+        // The actual result starts at dummyHead.next
+        return dummyHead.next;
     }
-
-    return dummy.next;
-  }  
-    
 }
